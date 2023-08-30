a8d513b5a99b7bf09c54e8402c759477b19fbe46
amightybeard <126688479+amightybeard@users.noreply.github.com>
2023-08-30 17:32:19 -0400

    Update test-gamethread-poster.py

--- test-gamethread-poster.py
+++ test-gamethread-poster.py
@@ -19,7 +19,6 @@ def fetch_team_record(team_name):
     with open('csv/nfl_standings.csv', 'r', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            print(row)  # This will print each row
             if row['Team'] == team_name:
                 wins = row['Wins']
                 losses = row['Losses']
@@ -48,16 +47,28 @@ def convert_datetime_to_natural_format(dt_string):
     return date_format, time_format
     
 def post_game_thread(away_team, home_team, week, date_time, stadium, gamecast_link):
-    date_str, time_str = convert_datetime_to_natural_format(date_time)
-    
-    away_team_record = fetch_team_record(away_team)
-    home_team_record = fetch_team_record(home_team)
-    
     headers = {
         'authorization': 'Bearer ' + SQUABBLES_TOKEN
     }
+
+    # Fetch the win-loss records
+    away_team_wins, away_team_losses, away_team_ties = fetch_team_record(away_team)
+    home_team_wins, home_team_losses, home_team_ties = fetch_team_record(home_team)
+
+    # Format the win-loss-tie records
+    away_team_record = f"{away_team_wins}-{away_team_losses}"
+    if away_team_ties != '0':
+        away_team_record += f"-{away_team_ties}"
+    
+    home_team_record = f"{home_team_wins}-{home_team_losses}"
+    if home_team_ties != '0':
+        home_team_record += f"-{home_team_ties}"
+
+    date_str, time_str = convert_datetime_to_natural_format(date_time)
+
     title = f"[Game Thread] {away_team} at {home_team} - Week {week} - {date_str} at {time_str}"
-    content = f"""## {away_team} ({away_team_record[0]}-{away_team_record[1]}) at {home_team} ({home_team_record[0]}-{home_team_record[1]})
+
+    content = f"""## {away_team} ({away_team_record}) at {home_team} ({home_team_record})
 
 ---------
 
@@ -71,7 +82,6 @@ def post_game_thread(away_team, home_team, week, date_time, stadium, gamecast_li
 | **{away_team}** | 0 | 0 | 0 | 0 | 0 |"""
 
     resp = requests.post('https://squabblr.co/api/new-post', data={
-        "community_id": 22,
         "community_name": "NFL",
         "title": title,
         "content": content
