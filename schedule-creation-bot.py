ff4fd4d0aa5c3f4cb59f883f280e6666a935b8a3
amightybeard <126688479+amightybeard@users.noreply.github.com>
2023-08-30 12:02:13 -0400

    Update schedule-creation-bot.py

--- schedule-creation-bot.py
+++ schedule-creation-bot.py
@@ -1,6 +1,13 @@
-
 import requests
 import csv
+import os
+import json
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+GIST_ID = "ef63fd2037741d41c2209b46da0779b8"
+GITHUB_TOKEN = os.environ.get('NFLBOT_WRITE_TO_GIST')
 
 def fetch_nfl_schedule_for_week(week_number):
     # Define the API URL
@@ -33,18 +40,48 @@ def fetch_nfl_schedule_for_week(week_number):
     
     return games
 
+def update_gist_with_schedule(games):
+    # Convert the games list to CSV format
+    csv_data = "Week,Date & Time,Stadium,Home Team,Away Team,Gamecast Link\n"
+    for game in games:
+        csv_data += f"{game['Week']},{game['Date & Time']},{game['Stadium']},{game['Home Team']},{game['Away Team']},{game['Gamecast Link']}\n"
+    
+    # Define the API URL for the Gist
+    gist_url = f"https://api.github.com/gists/{GIST_ID}"
+    
+    # Define the headers, including authentication
+    headers = {
+        "Authorization": f"token {GITHUB_TOKEN}",
+        "Content-Type": "application/json"
+    }
+    
+    # Define the payload
+    payload = {
+        "files": {
+            "nfl-schedule.csv": {
+                "content": csv_data
+            }
+        }
+    }
+    
+    # Make the API request to update the Gist
+    response = requests.patch(gist_url, headers=headers, json=payload)
+    
+    return response.status_code == 200
+
 def main():
     # Loop through all weeks (1 to 18) and fetch the schedule
     all_games = []
     for week in range(1, 19):
         all_games.extend(fetch_nfl_schedule_for_week(week))
     
-    # Write the fetched data to csv/nfl-schedule.csv
-    with open('csv/nfl-schedule.csv', 'w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=['Week', 'Date & Time', 'Stadium', 'Home Team', 'Away Team', 'Gamecast Link'])
-        writer.writeheader()
-        for game in all_games:
-            writer.writerow(game)
+    # Update the Gist with the fetched schedule
+    success = update_gist_with_schedule(all_games)
+    
+    if success:
+        print("Successfully updated the Gist!")
+    else:
+        print("Failed to update the Gist.")
 
 if __name__ == "__main__":
     main()
