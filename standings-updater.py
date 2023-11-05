d817c632bd1ea97d51d06a8fab2e02aee8bcdb4f
amightybeard <126688479+amightybeard@users.noreply.github.com>
2023-11-05 09:15:56 -0500

    Update standings-updater.py

--- standings-updater.py
+++ standings-updater.py
@@ -1,27 +1,61 @@
 import requests
-import pandas as pd
+import csv
+import io
+import os
 
 # Constants
-ESPN_NFL_STANDINGS_ENDPOINT = "https://site.web.api.espn.com/apis/site/v2/sports/football/nfl/standings"
+ESPN_API_URL = "https://cdn.espn.com/core/nfl/standings?xhr=1"
+GITHUB_API_URL = "https://api.github.com"
+GITHUB_TOKEN = os.environ.get('NFLBOT_WRITE_TO_GIST')
+GIST_ID_STANDINGS = os.environ.get('NFLBOT_STANDINGS_GIST')
+GIST_FILENAME_STANDINGS = 'nfl-standings.csv'
 
-def fetch_espn_standings():
-    """
-    Fetch the NFL standings from ESPN.
-    """
-    response = requests.get(ESPN_NFL_STANDINGS_ENDPOINT)
-    response.raise_for_status()  # Raise an error if the request failed
+def fetch_nfl_standings(url):
+    response = requests.get(url)
+    response.raise_for_status()
     return response.json()
 
-def update_standings_csv(standings_data):
-    # Parse the fetched data to extract the required fields
-    # Use pandas to update the nfl-standings.csv file
-    pass
+def parse_standings_data(standings_data):
+    standings_list = []
+    for group in standings_data['content']['standings']['groups']:
+        conference, division = group['name'].split(' ', 1)
+        for team_entry in group['standings']['entries']:
+            team_name = team_entry['team']['displayName']
+            stats = [stat['displayValue'] for stat in team_entry['stats'][:4]]
+            standings_list.append([conference, division, team_name] + stats)
+    return standings_list
 
-def format_standings_for_squabblr(standings_data):
-    # Format the parsed standings data into the structure for Squabblr
-    pass
+def create_csv_content(standings_data):
+    headers = ['Conference', 'Division', 'Team', 'Wins', 'Losses', 'Ties', 'Win %']
+    csv_output = io.StringIO()
+    csv_writer = csv.writer(csv_output)
+    csv_writer.writerow(headers)
+    for row in standings_data:
+        csv_writer.writerow(row)
+    return csv_output.getvalue()
 
-if __name__ == "__main__":
-    standings_data = fetch_espn_standings()
-    update_standings_csv(standings_data)
-    format_standings_for_squabblr(standings_data)
+def update_gist(github_token, gist_id, file_name, content):
+    headers = {
+        'Authorization': f'token {github_token}',
+        'Accept': 'application/vnd.github.v3+json',
+    }
+    url = f"{GITHUB_API_URL}/gists/{gist_id}"
+    data = {
+        'files': {
+            file_name: {
+                'content': content
+            }
+        }
+    }
+    response = requests.patch(url, headers=headers, json=data)
+    response.raise_for_status()
+
+# Run the update process
+try:
+    standings_data = fetch_nfl_standings(ESPN_API_URL)
+    standings_list = parse_standings_data(standings_data)
+    csv_content = create_csv_content(standings_list)
+    update_gist(GITHUB_TOKEN, GIST_ID_STANDINGS, GIST_FILENAME_STANDINGS, csv_content)
+    print("The Gist has been updated successfully.")
+except requests.RequestException as e:
+    print(f"An error occurred: {e}")
